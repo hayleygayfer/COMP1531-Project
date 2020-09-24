2ef869d92dd1e54c21f1812e9b1ffeeb472da0f0
z5312096 <hayley@gayfer.id.au>
2020-09-24 16:08:37 +1000

    created user storage structure, started writing auth_register

--- src/auth.py
+++ src/auth.py
@@ -1,3 +1,16 @@
+state = {
+    users = [
+        {
+            'u_id': 1
+            'token': 123456
+            'email': "email@email.com"
+            'password': "password"
+            'name_first': "firstname"
+            'name_last': "lastname"
+        }
+    ]
+}
+
 def auth_login(email, password):
     return {
         'u_id': 1,
@@ -10,6 +23,17 @@ def auth_logout(token):
     }
 
 def auth_register(email, password, name_first, name_last):
+    state[users].append(
+        {
+            'u_id': len(state[users]) + 1
+            'token': ''
+            'email': email
+            'password': password 
+            'name_first': name_first
+            'name_last': name_last  
+        }
+    )
+
     return {
         'u_id': 1,
         'token': '12345',
