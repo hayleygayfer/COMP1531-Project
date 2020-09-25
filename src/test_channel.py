f16628e597e452af7da29e67181200f38cdd9ac7
Rohan Warrier <z5312990@ad.unsw.edu.au>
2020-09-25 14:37:05 +1000

    wrote some tests for channel_join

--- src/test_channel.py
+++ src/test_channel.py
@@ -36,6 +36,9 @@ def test_channel_invite():
     assert channel.channel_invite(token, ch_id2, trump_id) == False
     assert channel.channel_invite(token, ch_id, trump_id) == True
 
+    # Inviting people to a channel you're not a part of
+
+
 def test_channel_details():
     pass
 
@@ -45,8 +48,54 @@ def test_channel_messages():
 def test_channel_leave():
     pass
 
+# channel_join returns True if success (SUBJECT TO CHANGE)
 def test_channel_join():
-    pass
+    # Get a user to create a public channel
+    auth.auth_register("master@public.com", "iamthemaster", "Master", "Channel")
+    auth.auth_login("master@public.com", "iamthemaster")
+    token = [ user['token'] for user in data['users'] if user['email'] == 'master@public.com' ][0]
+    ch_id = channels.channels_create(token, "PublicChannel", "public")
+    auth.auth_logout(token)
+
+    # Check whether a new user can join this channel
+    auth.auth_register("servant@join.com", "iamaservant", "Channel", "Joiner")
+    auth.auth_login("servant@join.com", "iamaservant")
+    token = [ user['token'] for user in data['users'] if user['email'] == 'servant@join.com' ][0]
+    assert channel.channel_join(token, ch_id) == True
+
+    auth.auth_register("newbie@dummies.com", "password", "Newbie", "Rick")
+    auth.auth_login("newbie@dummies.com", "password")
+    token = [ user['token'] for user in data['users'] if user['email'] == 'newbie@dummies.com' ][0]
+    assert channel.channel_join(token, ch_id) == True
+
+    # Joining a private channel
+    private_ch_id = channels.channels_create(token, "NewbiePrivate", "private")
+    token = [ user['token'] for user in data['users'] if user['email'] == 'master@public.com' ][0]
+    assert channel.channel_join(token, private_ch_id) == False
+    token = [ user['token'] for user in data['users'] if user['email'] == 'servant@join.com' ][0]
+    assert channel.channel_join(token, private_ch_id) == False
+
+    # Joining a channel you're already in
+    assert channel.channel_join(token, ch_id) == False
+    token = [ user['token'] for user in data['users'] if user['email'] == 'newbie@dummies.com' ][0]
+    assert channel.channel_join(token, ch_id) == False
+
+    # Leaving a public channel then joining again
+    channel.channel_leave(token, ch_id)
+    assert channel.channel_join(token, ch_id) == True
+    token = [ user['token'] for user in data['users'] if user['email'] == 'servant@join.com' ][0]
+    channel.channel_leave(token, ch_id)
+    assert channel.channel_join(token, ch_id) == True
+
+    # Leaving a private channel then joining again
+    token = [ user['token'] for user in data['users'] if user['email'] == 'newbie@dummies.com' ][0]
+    master_id = [ person['u_id'] for person in data['users'] if person['email'] == 'master@public.com' ][0]
+    channel.channel_invite(token, private_ch_id, master_id)
+    
+    token = [ user['token'] for user in data['users'] if user['email'] == 'master@public.com' ][0]
+    channel.channel_leave(token, private_ch_id)
+    assert channel.channel_join(token, private_ch_id) == False
+
 
 def test_channel_addowner():
     pass
