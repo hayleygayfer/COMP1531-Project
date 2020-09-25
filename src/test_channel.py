8baef435e3e3b45660cf1afa0946ad76676cdf99
Rohan Warrier <z5312990@ad.unsw.edu.au>
2020-09-25 13:44:12 +1000

    located user data needed for testing

--- src/test_channel.py
+++ src/test_channel.py
@@ -1,21 +1,23 @@
 import auth
 import channel
 import channels
+from data import data
 
 # channel_invite returns True if success (SUBJECT TO CHANGE)
 def test_channel_invite():
     # A user creates a channel
     auth.auth_register("channelcreator@unsw.com", "pass1234", "Channel", "Creator")
-    ''' token = <insert code to grab token> '''
+    auth.auth_login("channelcreator@unsw.com", "pass1234")
+    token = [ person['token'] for person in data['users'] if person['email'] == 'channelcreator@unsw.com' ][0]
     ch_id = channels.channels_create(token, "ValidChannelName", "public")
     
     # Invite other users
     auth.auth_register("randomuser@gmail.com", "qwerty", "Random", "User")
-    ''' random_id = <insert code to grad u_id of random user> '''
+    random_id = [ person['u_id'] for person in data['users'] if person['email'] == 'randomuser@gmail.com' ][0]
     assert channel.channel_invite(token, ch_id, random_id) == True
 
     # Invite a user that already exists in the channel
-    ''' creator_id = <insert code to grab u_id of channel creator> '''
+    creator_id = [ person['u_id'] for person in data['users'] if person['email'] == 'channelcreator@unsw.com' ][0]
     assert channel.channel_invite(token, ch_id, creator_id) == False # Can't invite yourself
     assert channel.channel_invite(token, ch_id, random_id) == False
 
@@ -26,15 +28,13 @@ def test_channel_invite():
     # Invite valid user to a non-existant channel
     assert channel.channel_invite(token, 75385, random_id) == False
 
-    # Create a new channel and invite a new user to that channel
-    ch_id2 = channels.channels_create(token, "AnotherChannelName", "public")
+    # Create a new private channel and invite a new user to that channel
+    ch_id2 = channels.channels_create(token, "AnotherChannelName", "private")
     auth.auth_register("donaldtrump@america.com", "idklol", "Donald", "Trump")
-    ''' trump_id = <insert code to grab trump's u_id> '''
+    trump_id = [ person['u_id'] for person in data['users'] if person['email'] == 'donaldtrump@america.com' ][0]
     assert channel.channel_invite(token, ch_id2, trump_id) == True
     assert channel.channel_invite(token, ch_id2, trump_id) == False
     assert channel.channel_invite(token, ch_id, trump_id) == True
-
-    # Private channel??
 
 def test_channel_details():
     pass
