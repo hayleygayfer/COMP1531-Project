11e4e36c9549f5adedd57c1e18056033c6f24d8f
Rohan Warrier <z5312990@ad.unsw.edu.au>
2020-09-25 15:06:36 +1000

    more comments to make test easier to follow

--- src/test_channel.py
+++ src/test_channel.py
@@ -5,38 +5,82 @@ from data import data
 
 # channel_invite returns True if success (SUBJECT TO CHANGE)
 def test_channel_invite():
-    # A user creates a channel
-    auth.auth_register("channelcreator@unsw.com", "pass1234", "Channel", "Creator")
-    auth.auth_login("channelcreator@unsw.com", "pass1234")
-    token = [ person['token'] for person in data['users'] if person['email'] == 'channelcreator@unsw.com' ][0]
-    ch_id = channels.channels_create(token, "ValidChannelName", "public")
-    
-    # Invite other users
-    auth.auth_register("randomuser@gmail.com", "qwerty", "Random", "User")
-    random_id = [ person['u_id'] for person in data['users'] if person['email'] == 'randomuser@gmail.com' ][0]
-    assert channel.channel_invite(token, ch_id, random_id) == True
+    # Person1 creates a channel
+    auth.auth_register("person1@unsw.com", "pass1234", "Person", "One")
+    auth.auth_login("person1@unsw.com", "pass1234")
+    person1_id = [ person['u_id'] for person in data['users'] if person['email'] == 'person1@unsw.com' ][0]
+    token = [ person['token'] for person in data['users'] if person['email'] == 'person1@unsw.com' ][0]
+    ch_id = channels.channels_create(token, "MainChannel", "public")
 
-    # Invite a user that already exists in the channel
-    creator_id = [ person['u_id'] for person in data['users'] if person['email'] == 'channelcreator@unsw.com' ][0]
-    assert channel.channel_invite(token, ch_id, creator_id) == False # Can't invite yourself
-    assert channel.channel_invite(token, ch_id, random_id) == False
+    '''
+    Users: Person1
+    ch_id (Public) Members: Person1 (O)
+    '''
+    
+    # Person1 invites other users (Person2)
+    auth.auth_register("person2@gmail.com", "qwerty", "Person", "Two")
+    person2_id = [ person['u_id'] for person in data['users'] if person['email'] == 'person2@gmail.com' ][0]
+    assert channel.channel_invite(token, ch_id, person2_id) == True
+
+    '''
+    Users: Person1, Person2
+    ch_id (Public) Members: Person1 (O), Person2
+    '''
+
+    # Person1 invite a user that already exists in the channel
+    assert channel.channel_invite(token, ch_id, person1_id) == False # Can't invite yourself
+    assert channel.channel_invite(token, ch_id, person2_id) == False # Person2 already exists in channel
 
     # Invite a non-existant user to that channel
     assert channel.channel_invite(token, ch_id, 31415926) == False
     assert channel.channel_invite(token, ch_id, 4243) == False
 
     # Invite valid user to a non-existant channel
-    assert channel.channel_invite(token, 75385, random_id) == False
+    assert channel.channel_invite(token, 75385, person2_id) == False
 
     # Create a new private channel and invite a new user to that channel
-    ch_id2 = channels.channels_create(token, "AnotherChannelName", "private")
+    ch2_id = channels.channels_create(token, "PrivateChannel", "private")
     auth.auth_register("donaldtrump@america.com", "idklol", "Donald", "Trump")
     trump_id = [ person['u_id'] for person in data['users'] if person['email'] == 'donaldtrump@america.com' ][0]
-    assert channel.channel_invite(token, ch_id2, trump_id) == True
-    assert channel.channel_invite(token, ch_id2, trump_id) == False
+    
+    '''
+    Users: Person1, Person2, Trump
+    ch_id (Public) Members: Person1 (O), Person2
+    ch2_id (Private) Members: Person1 (O)
+    '''    
+    
+    assert channel.channel_invite(token, ch2_id, trump_id) == True
+    assert channel.channel_invite(token, ch2_id, trump_id) == False # Invite twice error
     assert channel.channel_invite(token, ch_id, trump_id) == True
 
-    # Inviting people to a channel you're not a part of
+    channel.channel_addowner(token, ch_id, person2_id)
+
+    '''
+    Users: Person1, Person2, Trump
+    ch_id (Public) Members: Person1 (O), Person2 (O), Trump
+    ch2_id (Private) Members:  Person1 (O), Trump
+    '''
+
+    # Inviting people depending on your owner status
+    auth.auth_register("person3@hotmail.com", "lonelysad", "Person", "Three")
+    person3_id = [ person['u_id'] for person in data['users'] if person['email'] == 'person3@hotmail.com' ][0]
+
+    '''
+    Users: Person1, Person2, Trump, Person3
+    ch_id (Public) Members: Person1 (O), Person2 (O), Trump
+    ch2_id (Private) Members:  Person1 (O), Trump
+    '''
+
+    token = [ person['token'] for person in data['users'] if person['email'] == 'person2@gmail.com' ][0]
+    assert channel.channel_invite(token, ch_id, person3_id) == True # Person2 can invite Person3 to ch_id
+    token = [ person['token'] for person in data['users'] if person['email'] == 'donaldtrump@america.com' ][0]
+    assert channel.channel_invite(token, ch_id, person3_id) == True # Trump can't invite Person3 to ch2_id
+
+    '''
+    Users: Person1, Person2, Trump, Person3
+    ch_id (Public) Members: Person1 (O), Person2 (O), Trump, Person3
+    ch2_id (Private) Members:  Person1 (O), Trump
+    '''
 
 
 def test_channel_details():
