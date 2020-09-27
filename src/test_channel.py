0c4103d3ebb8b9d644e2f72309e26897d8284812
Rohan Warrier <z5312990@ad.unsw.edu.au>
2020-09-27 12:54:56 +1000

    a better way to access u_id / token

--- src/test_channel.py
+++ src/test_channel.py
@@ -4,27 +4,18 @@ import channels
 import pytest
 
 from error import InputError, AccessError
-from data import data
 from other import clear
 
-# Returns either the token or u_id depending on first parameter 
-def get_info_auth(data_type, email):
-    return [ user[data_type] for user in data['users'] if user['email'] == email ][0]
 
-
-# channel_invite returns True if success (SUBJECT TO CHANGE)
+# channel_invite returns a dictionary {is_success: True/False}
 def test_channel_invite_InputErrors():
     clear()
     # Create users
-    auth.auth_register("person1@unsw.com", "pass1234", "Person", "One")
-    auth.auth_login("person1@unsw.com", "pass1234")
-    person1_id = get_info_auth('u_id', 'person1@unsw.com')
-    p1_token = get_info_auth('token', 'person1@unsw.com')
+    person1_id = auth.auth_register("person1@unsw.com", "pass1234", "Person", "One")['u_id']
+    p1_token = auth.auth_login("person1@unsw.com", "pass1234")['token']
 
-    auth.auth_register("person2@gmail.com", "qwerty", "Person", "Two")
-    person2_id = get_info_auth('u_id','person2@gmail.com')
-    auth.auth_register("donaldtrump@america.com", "idklol", "Donald", "Trump")
-    trump_id = get_info_auth('u_id', 'donaldtrump@america.com')
+    person2_id = auth.auth_register("person2@gmail.com", "qwerty", "Person", "Two")['u_id']
+    trump_id = auth.auth_register("donaldtrump@america.com", "idklol", "Donald", "Trump")['u_id']
 
     '''
     Users: Person1, Person2, Trump
@@ -32,7 +23,7 @@ def test_channel_invite_InputErrors():
 
     # Person1 creates a channel and invites other users
     ch_id = channels.channels_create(p1_token, "MainChannel", "public")
-    assert channel.channel_invite(p1_token, ch_id, person2_id) == True
+    assert channel.channel_invite(p1_token, ch_id, person2_id)['is_success'] == True
 
     '''
     Users: Person1, Person2, Trump
@@ -56,7 +47,7 @@ def test_channel_invite_InputErrors():
         channel.channel_invite(p1_token, 75385, person2_id)
 
     # Now finally invite Trump to that channel
-    assert channel.channel_invite(p1_token, ch_id, trump_id) == True
+    assert channel.channel_invite(p1_token, ch_id, trump_id)['is_success'] == True
     
     '''
     Users: Person1, Person2, Trump
@@ -67,23 +58,16 @@ def test_channel_invite_AccessErrors():
     clear()
     # Create users
     auth.auth_register("scottmorrison@auspm.com", "scomo", "Scott", "Morrison")
-    auth.auth_login("scottmorrison@auspm.com", "scomo")
-    scomo_token = get_info_auth('token', 'scottmorrison@auspm.com')
+    scomo_token = auth.auth_login("scottmorrison@auspm.com", "scomo")['token']
 
-    auth.auth_register("tonyabbott@auspm.com", "idontcare", "Tony", "Abbott")
-    auth.auth_login("tonyabbott@auspm.com", "idontcare")
-    abbott_id = get_info_auth('u_id', 'tonyabbott@auspm.com')
-    abbott_token = get_info_auth('token', 'tonyabbott@auspm.com')
+    abbott_id = auth.auth_register("tonyabbott@auspm.com", "idontcare", "Tony", "Abbott")['u_id']
+    abbott_token = auth.auth_login("tonyabbott@auspm.com", "idontcare")['token']
 
-    auth.auth_register("kevinrudd@auspm.com", "idontcare", "Kevin", "Rudd")
-    auth.auth_login("kevinrudd@auspm.com", "idontcare")
-    rudd_id = get_info_auth('u_id', 'kevinrudd@auspm.com')
-    rudd_token = get_info_auth('token', 'kevinrudd@auspm.com')
+    rudd_id = auth.auth_register("kevinrudd@auspm.com", "idontcare", "Kevin", "Rudd")['u_id']
+    rudd_token = auth.auth_login("kevinrudd@auspm.com", "idontcare")['token']
 
-    auth.auth_register("juliagillard@auspm.com", "idontcare", "Julia", "Gillard")
-    auth.auth_login("juliagillard@auspm.com", "idontcare")
-    gillard_id = get_info_auth('u_id', 'juliagillard@auspm.com')
-    gillard_token = get_info_auth('token', 'juliagillard@auspm.com')
+    gillard_id = auth.auth_register("juliagillard@auspm.com", "idontcare", "Julia", "Gillard")['u_id']
+    gillard_token = auth.auth_login("juliagillard@auspm.com", "idontcare")['token']
 
     # Scomo and Abbot creating channels
     pub_id = channels.channels_create(scomo_token, "PublicChannel", "public")
@@ -96,8 +80,8 @@ def test_channel_invite_AccessErrors():
     '''
 
     # Inviting other users
-    assert channel.channel_invite(abbott_token, prv_id, rudd_id) == True
-    assert channel.channel_invite(scomo_token, pub_id, gillard_id) == True
+    assert channel.channel_invite(abbott_token, prv_id, rudd_id)['is_success'] == True
+    assert channel.channel_invite(scomo_token, pub_id, gillard_id)['is_success'] == True
 
     '''
     Users: Scomo, Abbott, Rudd, Gillard
@@ -111,8 +95,8 @@ def test_channel_invite_AccessErrors():
     with pytest.raises(AccessError):
         channel.channel_invite(rudd_token, prv_id, abbott_id) # Rudd cannot invite Abbott to public (since Rudd is not a member)
     
-    assert channel.channel_invite(gillard_token, pub_id, rudd_id) == True # Gillard can invite Rudd to public
-    assert channel.channel_invite(rudd_token, prv_id, abbott_id) == True # NOW Rudd can invite Abbott
+    assert channel.channel_invite(gillard_token, pub_id, rudd_id)['is_success'] == True # Gillard can invite Rudd to public
+    assert channel.channel_invite(rudd_token, prv_id, abbott_id)['is_success'] == True # NOW Rudd can invite Abbott
 
     '''
     Users: Scomo, Abbott, Rudd, Gillard
@@ -127,26 +111,21 @@ def test_channel_details():
 def test_channel_messages():
     clear()
 
-# channel_leave returns True if success
+# channel_leave returns a dictionary {is_success: True/False}
 def test_channel_leave():
     clear()
     # Create users
     auth.auth_register("diglett@pokemon.com", "arenatrap", "Diglett", "Pokemon")
-    auth.auth_login("diglett@pokemon.com", "arenatrap")
-    diglett_token = get_info_auth('token', "diglett@pokemon.com")
+    diglett_token = auth.auth_login("diglett@pokemon.com", "arenatrap")['token']
 
-    auth.auth_register("ponyta@pokemon.com", "horndrillXD", "Ponyta", "Pokemon")
-    auth.auth_login("ponyta@pokemon.com", "horndrillXD")
-    ponyta_token = get_info_auth('token', "ponyta@pokemon.com")
-    ponyta_id = get_info_auth('u_id', "ponyta@pokemon.com")
+    ponyta_id = auth.auth_register("ponyta@pokemon.com", "horndrillXD", "Ponyta", "Pokemon")['u_id']
+    ponyta_token = auth.auth_login("ponyta@pokemon.com", "horndrillXD")['token']
 
     auth.auth_register("gyarados@pokemon.com", "notadragontype", "Gyarados", "Pokemon")
-    auth.auth_login("gyarados@pokemon.com", "notadragontype")
-    gyarados_token = get_info_auth('token', "gyarados@pokemon.com")
+    gyarados_token = auth.auth_login("gyarados@pokemon.com", "notadragontype")['token']
 
     auth.auth_register("dratini@pokemon.com", "notawatertype", "Dratini", "Pokemon")
-    auth.auth_login("dratini@pokemon.com", "notadragontype")
-    dratini_token = get_info_auth('token', "dratini@pokemon.com")
+    dratini_token = auth.auth_login("dratini@pokemon.com", "notawatertype")['token']
 
     # Users create and join channels
     moleID = channels.channels_create(diglett_token, "MoleChannel", "public")
@@ -160,7 +139,7 @@ def test_channel_leave():
     '''
 
     # Ensure Ponyta can leave
-    assert channel.channel_leave(ponyta_token, splashID) == True
+    assert channel.channel_leave(ponyta_token, splashID)['is_success'] == True
     channel.channel_join(ponyta_token, moleID)
 
     '''
@@ -184,29 +163,25 @@ def test_channel_leave():
     with pytest.raises(InputError):
         channel.channel_leave(diglett_token, moleID) # If diglett leaves there are no owners
     channel.channel_addowner(diglett_token, moleID, ponyta_id)
-    assert channel.channel_leave(diglett_token, moleID) == True # NOW Diglett can leave since Ponyta is an owner
+    assert channel.channel_leave(diglett_token, moleID)['is_success'] == True # NOW Diglett can leave since Ponyta is an owner
 
     '''
     MoleChannel: Ponyta (O)
     SplashChannel: Gyarados (O), Dratini
     '''
 
-# channel_join returns True if success (SUBJECT TO CHANGE)
+# channel_join returns a dictionary {is_success: True/False}
 def test_channel_join():
     clear()
     # Create users
-    auth.auth_register("mario@nintendo.com", "mammamia", "Mario", "Idk")
-    auth.auth_login("mario@nintendo.com", "mammamia")
-    mario_id = get_info_auth('u_id', 'mario@nintendo.com')
-    mario_token = get_info_auth('token', 'mario@nintendo.com')
+    mario_id = auth.auth_register("mario@nintendo.com", "mammamia", "Mario", "Idk")['mario_id']
+    mario_token = auth.auth_login("mario@nintendo.com", "mammamia")
 
     auth.auth_register("luigi@nintendo.com", "letsgo", "Luigi", "Idk")
-    auth.auth_login("luigi@nintendo.com", "letsgo")
-    luigi_token = get_info_auth('token', 'luigi@nintendo.com')
+    luigi_token = auth.auth_login("luigi@nintendo.com", "letsgo")['token']
 
     auth.auth_register("princesspeach@nintendo.com", "mushroom", "Peach", "Toadstool")
-    auth.auth_login("princesspeach@nintendo.com", "mushroom")
-    peach_token = get_info_auth('token', 'peach@nintendo.com')
+    peach_token = auth.auth_login("princesspeach@nintendo.com", "mushroom")['token']
 
     # Mario and Peach create a public and private channel respectively
     pub_ch_id = channels.channels_create(mario_token, "PublicChannel", "public")
@@ -219,8 +194,8 @@ def test_channel_join():
     '''
 
     # Check whether other users can join the public channel
-    assert channel.channel_join(luigi_token, pub_ch_id) == True
-    assert channel.channel_join(peach_token, pub_ch_id) == True
+    assert channel.channel_join(luigi_token, pub_ch_id)['is_success'] == True
+    assert channel.channel_join(peach_token, pub_ch_id)['is_success'] == True
 
     '''
     Users: Mario, Luigi, Peach
@@ -244,7 +219,7 @@ def test_channel_join():
 
     # Leaving a public channel then joining again
     channel.channel_leave(peach_token, pub_ch_id)
-    assert channel.channel_join(peach_token, pub_ch_id) == True # Peach can leave and enter again
+    assert channel.channel_join(peach_token, pub_ch_id)['is_success'] == True # Peach can leave and enter again
 
     # Leaving a private channel then joining again
     channel.channel_invite(peach_token, prv_ch_id, mario_id)
@@ -269,6 +244,7 @@ def test_channel_removeowner():
 
 # IGNORE THIS
 if __name__ == "__main__":
-    u_id = get_info_auth('u_id', 'random@email.com')
+    u_id = auth.auth_register("person1@unsw.com", "pass1234", "Person", "One")['u_id']
     print(u_id)
-    
+    u_id2 = auth.auth_register("person2@unsw.com", "pass1234", "Person", "One")['u_id']
+    print(u_id2)
