6e55661359379bb9be7119063f9fc18575a2927e
Ethan Hansen <e.hansen@student.unsw.edu.au>
2020-09-28 15:33:41 +1000

    Began implementing channels functions, refactored data structure for a channel

--- src/channels.py
+++ src/channels.py
@@ -1,24 +1,49 @@
+import data from data
+
 def channels_list(token):
+
+    channels = []
+    u_id = 0
+
+    # Check for authentication
+    for user in data['users']:
+        if user['token'] == token:
+            u_id = user['u_id']
+    
+    # Loop through channels, and associated users to find matches -> inefficient
+    for channel in data['channels']:
+        for member in channel['all_members']:
+            if member == u_id:
+                channels.append(channel)
+    
     return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
+        'channels': channels
     }
 
 def channels_listall(token):
+
     return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
+        'channels': data['channels']
     }
 
 def channels_create(token, name, is_public):
+    u_id = 0
+
+    # Check for authentication & retrieve owner member id
+    for user in data['users']:
+        if user['token'] == token:
+            u_id = user['u_id']
+
+    channel = {
+        'channel_id': len(data['channels']),
+        'name': name,
+        'is_public': is_public,
+        'all_members': [u_id],
+        'owner_members': [u_id]
+    }
+
+    data['channels'].append(channel)
+
     return {
-        'channel_id': 1,
+        'channel_id': channel['channel_id']
     }
