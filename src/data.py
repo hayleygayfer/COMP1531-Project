adf0087d0d8c350f4b9cd1de82f8c334e075e6b2
Ethan Hansen <e.hansen@student.unsw.edu.au>
2020-09-29 08:25:00 +1000

    Updated sample data structure to reflect agreed channels struct

--- src/data.py
+++ src/data.py
@@ -4,20 +4,30 @@ data = {
             'u_id': 1,
             'email': 'random@email.com',
             'password': 'password',
-            'token': '123456'
+            'token': '123456',
+            'name_first': 'John',
+            'name_last': 'Doe'
         }
     ],
     'channels': [
         {
-            'id': 1,
-            'name': 'channel1'
+            'channel_id': 1,
+            'name': 'channel1',
+            'all_members': [
+                {
+                    'u_id': 1,
+                    'name_first': 'John',
+                    'name_last': 'Doe'
+                }
+            ],
+            'owner_members': [
+                {
+                    'u_id': 1,
+                    'name_first': 'John',
+                    'name_last': 'Doe'
+                }
+            ],
+            'is_public': True
         }
     ]
 }
-
-def clear():
-    global data
-    data = {
-        'users': [],
-        'channels': []
-    }
