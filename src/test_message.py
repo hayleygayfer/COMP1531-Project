fafbc5cdaf540a86d2787f2c376e24eaed39dbc7
z5312096 <hayley@gayfer.id.au>
2020-10-11 18:41:00 +1100

    started writing formatting for tests

---
+++ src/test_message.py
@@ -0,0 +1,34 @@
+# test message.py
+
+from message import message_send, message_remove, message_edit
+
+### message ###
+
+# test message_send #
+"""
+message_send(token, channel_id, message)
+OUTPUT:
+"""
+
+# VALID CASES #
+
+
+# INVALID CASES #
+
+# test message_remove #
+"""
+
+"""
+
+# VALID CASES #
+
+# INVALID CASES #
+
+
+# test message_edit #
+
+# VALID CASES #
+
+# INVALID CASES #
+
+
