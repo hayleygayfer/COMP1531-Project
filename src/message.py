e853d59ec50ee353f4e3ba9cff18a014c912805e
Rohan Warrier <z5312990@ad.unsw.edu.au>
2020-10-19 14:34:06 +1100

    added more validation checks after meeting

--- src/message.py
+++ src/message.py
@@ -2,8 +2,6 @@ from data import data
 from error import InputError, AccessError
 
 MAX_MSG_IN_CH = 10000
-EDIT = 1
-REMOVE = 2
 
 def message_send(token, channel_id, message):
     '''Send a message from an authorised user to the channel specified by channel_id'''
@@ -44,15 +42,24 @@ def message_remove(token, message_id):
     if not valid_channel(channel_id) or message_not_found(channel_id, message_id):
         raise InputError("Message not found")
 
-    # Only the user or flockr owner can remove a message
-    msg_sender_id = find(message_id, channel_id, None, 0)
+    # Only a channel owner or flockr owner can remove a message
+    channel = find_channel(message_id, channel_id)
+    owners = channel['owner_members']
     flockr_owner_id = get_flockr_owner_id(u_id)
-    if msg_sender_id != u_id and msg_sender_id != flockr_owner_id:
-        raise AccessError("You cannot remove this message")
+    if user_is_not_owner(u_id, owners) and u_id != flockr_owner_id:
+        raise AccessError("You are not authorised to remove this message")
 
+    # an owner cannot remove another owner's message unless it's your own
+    for msg in channel['messages']:
+        if msg.get('message_id') == message_id:
+            msg_creator = msg['u_id']
+    if not user_is_not_owner(msg_creator, owners) or msg_creator == flockr_owner_id:
+        # trying to edit a channel/flockr owner's message
+        if msg_creator != u_id:
+            raise AccessError("You cannot remove another owner's message")
 
     # Remove the message
-    find(message_id, channel_id, None, REMOVE)
+    do_remove(channel, message_id)
     return {
         'is_success': True
     }
@@ -72,14 +79,24 @@ def message_edit(token, message_id, message):
     if not valid_channel(channel_id):
         raise InputError("Message not found")
 
-    # Only the user or flockr owner can remove a message
-    msg_sender_id = find(message_id, channel_id, None, 0)
+    # Only a channel owner or flockr owner can remove a message
+    channel = find_channel(message_id, channel_id)
+    owners = channel['owner_members']
     flockr_owner_id = get_flockr_owner_id(u_id)
-    if msg_sender_id != u_id and msg_sender_id != flockr_owner_id:
-        raise AccessError("You cannot edit this message")
+    if user_is_not_owner(u_id, owners) and u_id != flockr_owner_id:
+        raise AccessError("You are not authorised to edit this message")
+
+    # an owner cannot edit another owner's message unless it's your own
+    for msg in channel['messages']:
+        if msg.get('message_id') == message_id:
+            msg_creator = msg['u_id']
+    if not user_is_not_owner(msg_creator, owners) or msg_creator == flockr_owner_id:
+        # trying to edit a channel/flockr owner's message
+        if msg_creator != u_id:
+            raise AccessError("You cannot edit another owner's message")
 
     # Edit the message
-    find(message_id, channel_id, message, EDIT)
+    do_edit(channel, message_id, message)
     return {
         'is_success': True
     }
@@ -143,18 +160,29 @@ def append_msg_to_channel(channel_id, msg_string, msg_id, u_id, time):
             )
             channel['message_count'] += 1
 
-def find(msg_id, channel_id, new_msg, mode):
+def find_channel(msg_id, channel_id):
     for channel in data['channels']:
         if channel['channel_id'] == channel_id:
             for msg in channel['messages']:
                 if msg.get('message_id') == msg_id:
-                    # get mode to determine what to do
-                    if mode == EDIT:
-                        msg['message'] == new_msg
-                    elif mode == REMOVE:
-                        channel['messages'].remove(msg)
-                    else:
-                        return msg['u_id']
+                    return channel
+
+def do_edit(channel, msg_id, new_msg):
+    for msg in channel['messages']:
+        if msg.get('message_id') == msg_id:
+            msg['message'] == new_msg
+
+def do_remove(channel, msg_id):
+    for msg in channel['messages']:
+        if msg.get('message_id') == msg_id:
+            channel['messages'].remove(msg)
+
+
+def user_is_not_owner(u_id, owners):
+    for p in owners:
+        if p.get('u_id') == u_id:
+            return False
+    return True
 
 def get_flockr_owner_id(u_id):
     if u_id == data['users'][0].get('u_id'):
