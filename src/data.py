5d36447bc10c770b90199902f2a478e955b1ec2b
Rohan Warrier <z5312990@ad.unsw.edu.au>
2020-10-26 16:38:02 +1100

    data is empty by default

--- src/data.py
+++ src/data.py
@@ -1,47 +1,52 @@
 data = {
-    'users': [
-        {
-            'u_id': 1,
-            'email': 'random@email.com',
-            'password': 'password',
-            'token': '123456',
-            'name_first': 'John',
-            'name_last': 'Doe',
-            'handle_str': 'handle',
-        }
-    ],
-    'channels': [
-        {
-            'channel_id': 1,
-            'name': 'channel1',
-            'all_members': [
-                {
-                    'u_id': 1,
-                    'name_first': 'John',
-                    'name_last': 'Doe'
-                }
-            ],
-            'owner_members': [
-                {
-                    'u_id': 1,
-                    'name_first': 'John',
-                    'name_last': 'Doe'
-                }
-            ],
-            'is_public': True,
-            'messages': [
-                {
-                    'u_id': 1,
-                    'message_id': 10001,
-                    'message': "Hi, this is a simple test msg"
-                },
-                {
-                    'u_id': 2,
-                    'message_id': 10002,
-                    'message': "I got it!"
-                }
-            ],
-            'message_count': 2
-        }
-    ]
+    'users': [],
+    'channels': [],
 }
+
+'''
+'users': [
+    {
+        'u_id': 1,
+        'email': 'random@email.com',
+        'password': 'password',
+        'token': '123456',
+        'name_first': 'John',
+        'name_last': 'Doe',
+        'handle_str': 'handle',
+    }
+],
+'channels': [
+    {
+        'channel_id': 1,
+        'name': 'channel1',
+        'all_members': [
+            {
+                'u_id': 1,
+                'name_first': 'John',
+                'name_last': 'Doe'
+            }
+        ],
+        'owner_members': [
+            {
+                'u_id': 1,
+                'name_first': 'John',
+                'name_last': 'Doe'
+            }
+        ],
+        'is_public': True,
+        'messages': [
+            {
+                'u_id': 1,
+                'message_id': 10001,
+                'message': "Hi, this is a simple test msg"
+            },
+            {
+                'u_id': 2,
+                'message_id': 10002,
+                'message': "I got it!"
+            }
+        ],
+        'message_count': 2
+    }
+]
+'''
